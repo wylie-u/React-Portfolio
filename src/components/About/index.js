9f09c6f93d453aef0f5f16c9f4e56b629836c4ea
wylie-u <wylieurig@gmail.com>
2022-09-02 09:35:29 -0600

    stylings fixed but picture needs work

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,7 @@ function AboutMe() {
           <h2>Hi, I'm Wylie Urig.</h2>
           <p>
             I'm currently living, exploring and creating in Denver, CO. I'm a
-            junior full-stack developer looking to grow my skills and break a
+            junior .NET full-stack developer looking to grow my skills and break a
             few things along the way. Thanks for looking!{" "}
           </p>
         </article>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,44 +1,50 @@
 import React from "react";
 import "./style.css";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand-md nav-item navbar-light wy-nav">
-        <Link to="/about" className="navbar-brand wy-logo">W</Link> 
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md nav-item navbar-light wy-nav">
+        <Link to="/about" className="navbar-brand wy-logo">
+          W
+        </Link>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler collapse"
           type="button"
           data-toggle="collapse"
           data-target="#navbarTogglerDemo02"
           aria-controls="navbarTogglerDemo02"
           aria-expanded="false"
-          aria-label="Toggle navigation">
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-  
+
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             <li className="nav-item active ">
-            <Link to="/portfolio" className="nav-link nav-link2">Portfolio</Link> 
+              <Link to="/portfolio" className="nav-link nav-link2">
+                Portfolio
+              </Link>
             </li>
             <li className="nav-item">
-              <Link to="/resume" className="nav-link nav-link3">Resume</Link> 
+              <Link to="/resume" className="nav-link nav-link3">
+                Resume
+              </Link>
             </li>
             <li className="nav-item">
-            <Link to="/contact" className="nav-link nav-link4">Contact</Link> 
+              <Link to="/contact" className="nav-link nav-link4">
+                Contact
+              </Link>
             </li>
           </ul>
         </div>
       </nav>
-      </div>
-      
-    );
-  }
-  
-  export default Nav;
+    </div>
+  );
+}
 
-  // className="navbar-brand wy-logo"
+export default Nav;
 
-  
+// className="navbar-brand wy-logo"
